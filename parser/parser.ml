8fef787ec497b72eb6f1a02c00a4cc7538f00168
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2017-01-17 13:57:48 +0000

    More work on Church encoded datatypes

--- parser/parser.ml
+++ parser/parser.ml
@@ -110,11 +110,11 @@ let parser expr (m : mode) =
       when m = `Prp`A
       -> in_pos _loc (EDSum(fs))
   (* Proposition (universal quantification) *)
-  | "∀" '(' x:llid ":" s:sort ")" a:(expr (`Prp`F))
+  | "∀" '(' x:llid ":" s:sort ')' a:(expr (`Prp`F))
       when m = `Prp`F
       -> in_pos _loc (EUniv(x,s,a))
   (* Proposition (existential quantification) *)
-  | "∃" '(' x:llid ":" s:sort ")" a:(expr (`Prp`F))
+  | "∃" '(' x:llid ":" s:sort ')' a:(expr (`Prp`F))
       when m = `Prp`F
       -> in_pos _loc (EExis(x,s,a))
   (* Proposition (least fixpoint) *)
