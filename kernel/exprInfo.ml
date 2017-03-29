1d973d2ad6741abe9bd2682b6d79c369cfa570b5
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2017-03-29 20:26:37 +0000

    List of ordinals (first step toward abstract sequents)

--- kernel/exprInfo.ml
+++ kernel/exprInfo.ml
@@ -74,13 +74,82 @@ let isTerm : type a.a ex loc -> t ex loc option = fun e ->
   | (T,e) -> Some e
   | _     -> None
 
+type ('a, 'b) mbndr = ('a ex loc, 'b ex loc) mbinder
 
-(*
-let rec term_has_type : term -> bool = fun t ->
-  let t = Norm.whnf t in
-  match t with
-  | Appl(t,_) -> term_has_type t
-  | (V,e)alu v -> valu_has_type v
+let generalise : type a. a ex loc -> (o, a) mbndr * ordi array = fun e ->
+  let rec owits : type a. ordi list -> a ex loc -> ordi list = fun acc e ->
+    let from_cond acc c =
+      match c with
+      | Equiv(t,_,u) -> owits (owits acc t) u
+      | Posit(o)     -> owits acc o
+      | NoBox(v)     -> owits acc v
+    in
+    match (Norm.whnf e).elt with
+    | HDef(_,_)   -> []
+    | HApp(_,f,a) -> owits (owits acc f) a
+    | HFun(_,_,f) -> owits acc (bndr_subst f Dumm)
+    | UWit(s,t,a) ->
+        begin
+          match s with
+          | O -> if List.memq e acc then e :: acc else acc
+          | _ -> owits (owits acc t) (bndr_subst a Dumm)
+        end
+    | EWit(s,t,a) ->
+        begin
+          match s with
+          | O -> if List.memq e acc then e :: acc else acc
+          | _ -> owits (owits acc t) (bndr_subst a Dumm)
+        end
+    | UVar(_,_)   -> []
+    | ITag(_,_)   -> []
+    | Goal(_,_)   -> []
 
-and value_has_type
- *)
+    | Func(a,b)   -> owits (owits acc a) b
+    | Prod(m)     -> A.fold (fun _ (_,a) acc -> owits acc a) m acc
+    | DSum(m)     -> A.fold (fun _ (_,a) acc -> owits acc a) m acc
+    | Univ(_,f)   -> owits acc (bndr_subst f Dumm)
+    | Exis(_,f)   -> owits acc (bndr_subst f Dumm)
+    | FixM(o,f)   -> owits (owits acc o) (bndr_subst f Dumm)
+    | FixN(o,f)   -> owits (owits acc o) (bndr_subst f Dumm)
+    | Memb(t,a)   -> owits (owits acc t) a
+    | Rest(a,c)   -> owits (from_cond acc c) a
+    | Impl(c,a)   -> owits (from_cond acc c) a
+
+    | VWit(t,a,b) -> owits (owits (owits acc
+                       (bndr_subst t Dumm)) (bndr_subst a Dumm)) b
+    | LAbs(_,f)   -> owits acc (bndr_subst f Dumm)
+    | Cons(_,v)   -> owits acc v
+    | Reco(m)     -> A.fold (fun _ (_,v) acc -> owits acc v) m acc
+    | Scis        -> []
+    | VDef(_)     -> []
+    | VTyp(v,_)   -> owits acc v
+    | VLam(_,f)   -> owits acc (bndr_subst f Dumm)
+
+    | Valu(v)     -> owits acc v
+    | Appl(t,u)   -> owits (owits acc t) u
+    | MAbs(f)     -> owits acc (bndr_subst f Dumm)
+    | Name(s,t)   -> owits (owits acc s) t
+    | Proj(v,_)   -> owits acc v
+    | Case(v,m)   -> let fn _ (_,f) acc = owits acc (bndr_subst f Dumm) in
+                     A.fold fn m (owits acc v)
+    | FixY(t,v)   -> owits (owits acc t) v
+    | TTyp(t,_)   -> owits acc t
+    | TLam(_,f)   -> owits acc (bndr_subst f Dumm)
+
+    | Epsi        -> []
+    | Push(v,s)   -> owits (owits acc v) s
+    | Fram(t,s)   -> owits (owits acc t) s
+    | SWit(f,a)   -> owits (owits acc (bndr_subst f Dumm)) a
+
+    | Conv        -> []
+    | Succ(o)     -> owits acc o
+    | OWit(_)     -> if List.memq e acc then e :: acc else acc
+
+    | Vari _      -> []
+    | Dumm        -> []
+  in
+  let os = owits [] e in
+
+  (* TODO *)
+  ignore os;
+  assert false
