33bc058534de59fd14dbb421afdecfc520bf7a1a
craff <christophe@raffalli.eu>
2020-02-21 07:09:34 -1000

    use latest eq_closure

--- src/util/ahash.ml
+++ src/util/ahash.ml
@@ -11,8 +11,52 @@ type ('a, 'b) t =
   ; initial_size : int }                     (* Initial array size. *)
 
 (* Full comparision function (compares closures with physical equality). *)
-let compare x y =
-  try compare x y = 0 with _ -> x == y
+(** [eq_closure] is an alternative to the polymorphic equality function [(=)],
+    that compares closures using [(==)] instead of failing. Note that equality
+    testing is consequently not perfect. *)
+let eq_closure : type a. a -> a -> bool = fun v1 v2 ->
+  (* We remember encountered values in [eq_done] to handle cyclicity. *)
+  let eq_done : (Obj.t * Obj.t) list ref = ref [] in
+  let rec eq : Obj.t -> Obj.t -> bool = fun v1 v2 ->
+    (* Physical equality is tested first. *)
+    v1 == v2 ||
+    (* We then look at tags, and unfold potential forward blocks. *)
+    let t1 = Obj.tag v1 in
+    if t1 = Obj.forward_tag then eq (Obj.field v1 0) v2 else
+    let t2 = Obj.tag v2 in
+    if t2 = Obj.forward_tag then eq v1 (Obj.field v2 0) else
+    (* Tags must otherwise be the same to have equality. *)
+    t1 == t2 &&
+    (* Strings, doubles and arrays of doubles are compared using [=]. *)
+    if t1 = Obj.string_tag then v1 = v2 else
+    if t1 = Obj.double_tag then v1 = v2 else
+    if t1 = Obj.double_array_tag then v1 = v2 else
+    (* For everything that is not a non-constant constructors, equality failed
+    at this point (e.g., for closures or sealed values).  Such values are only
+    considered equal if physical equality succeeds (it was tested already). *)
+    Obj.first_non_constant_constructor_tag <= t1 &&
+    t1 <= Obj.last_non_constant_constructor_tag &&
+    Obj.size v1 == Obj.size v2 && (* Number of fields should be equal. *)
+    (* We recursively explore the fields. *)
+    let rec fn = function
+      | (v1', v2')::l ->
+         begin
+           match (v1 == v1', v2 == v2') with
+           | (true , true ) -> true
+           | (true , false) -> false
+           | (false, true ) -> false
+           | (_    , _    ) -> fn l
+         end
+      | []            ->
+         let rec gn i =
+           i < 0 || (eq (Obj.field v1 i) (Obj.field v2 i) && gn (i-1))
+         in
+         eq_done := (v1, v2) :: !eq_done; gn (Obj.size v1 - 1)
+    in
+    fn !eq_done
+  in
+  eq (Obj.repr v1) (Obj.repr v2)
+
 
 (* Creates a fresh, empty table. *)
 let create initial_size =
@@ -76,7 +120,7 @@ let remove h key =
   let rec remove_bucket = function
     | Nil              -> Nil
     | Cons(k, i, next) ->
-        if compare k key then (h.size <- h.size - 1; next)
+        if eq_closure k key then (h.size <- h.size - 1; next)
         else Cons(k, i, remove_bucket next)
   in
   let i = key_index h key in
@@ -84,33 +128,33 @@ let remove h key =
 
 let rec find_rec key = function
   | Nil              -> raise Not_found
-  | Cons(k, d, rest) -> if compare key k then d else find_rec key rest
+  | Cons(k, d, rest) -> if eq_closure key k then d else find_rec key rest
 
 let find h key =
   match h.data.(key_index h key) with
   | Nil                 -> raise Not_found
   | Cons(k1, d1, rest1) ->
-      if compare key k1 then d1 else
+      if eq_closure key k1 then d1 else
       match rest1 with
       | Nil                 -> raise Not_found
       | Cons(k2, d2, rest2) ->
-          if compare key k2 then d2 else
+          if eq_closure key k2 then d2 else
           match rest2 with
           | Nil                 -> raise Not_found
           | Cons(k3, d3, rest3) ->
-              if compare key k3 then d3 else find_rec key rest3
+              if eq_closure key k3 then d3 else find_rec key rest3
 
 let find_all h key =
   let rec find_in_bucket = function
     | Nil              -> []
-    | Cons(k, d, rest) -> if compare k key then d :: find_in_bucket rest
+    | Cons(k, d, rest) -> if eq_closure k key then d :: find_in_bucket rest
                           else find_in_bucket rest
   in find_in_bucket h.data.(key_index h key)
 
 let replace h key info =
   let rec replace_bucket = function
     | Nil              -> raise Not_found
-    | Cons(k, i, next) -> if compare k key then Cons(key, info, next)
+    | Cons(k, i, next) -> if eq_closure k key then Cons(key, info, next)
                           else Cons(k, i, replace_bucket next)
   in
   let i = key_index h key in
@@ -124,7 +168,7 @@ let replace h key info =
 let mem h key =
   let rec mem_in_bucket = function
   | Nil              -> false
-  | Cons(k, _, rest) -> compare k key || mem_in_bucket rest
+  | Cons(k, _, rest) -> eq_closure k key || mem_in_bucket rest
   in mem_in_bucket h.data.(key_index h key)
 
 let iter f h =
